7ab606827bd33ff5884ccfddb745a8bfa39ae415
Gubanova Valeria <gubanovavaleria07@gmail.com>
2023-12-08 17:28:49 +0600

    Добален текст подлючения сервисов, добавлены 2 виджета Сберпрайм и Переводы

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,3 @@
-
-
-//import 'dart:html';
 import 'dart:io' as i;
 
 import 'package:flutter/material.dart';
@@ -47,7 +44,8 @@ class MyHomePage extends StatelessWidget {
           ]
       ),
 
-      body: Center (child: Column (
+      body: Center (
+          child: Column (
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
           Image.asset('assets/images/img.png', width: 160, height: 160),
@@ -73,22 +71,117 @@ class MyHomePage extends StatelessWidget {
                 ]
             ),
             Container (
-                color: const Color(0x00000000),
+                margin: const EdgeInsets.only(left: 16, top: 30, right: 16, bottom: 0),
+                color: const Color(0x0032d7d2),
                 child: const Column(
                     children: <Widget>[
-                      SizedBox(height: 14),
                   Text(
                       'У вас подключено',
-                      style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+                      style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
                       textAlign: TextAlign.left
-                  )
+                  ),
+                      Text(
+                          'Подписки, автоплатежи и сервисы на которые вы подписались',
+                          style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w500,
+                              color: Color(0x8C000000) ),
+                          textAlign: TextAlign.left
+                      ),
+                ]
+                )
+      ),
+          SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              padding: const EdgeInsets.all(16.0),
+              child:Row(
+             children: <Widget>[
+         Container(
+             margin: const EdgeInsets.only(left: 20, top: 44, right: 50, bottom: 0),
+            child:  Column (
+                children: <Widget>[
+                 Row (
+                children: <Widget>[
+                  Image.asset('assets/images/sberprime.png', width: 40, height: 40),
+                  const SizedBox(width: 20),
+                  const Text(
+                    'СберПрайм',
+                    style: TextStyle(fontSize: 18.0,
+                    fontWeight: FontWeight.w500,
+                    ),
+                    textAlign: TextAlign.left
+                    )  ] ),
+                  const SizedBox(height: 22),
+                  const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start ,
+                      children: <Widget>[
+                        Text(
+                            'Платёж 9 июля',
+                            style: TextStyle(fontSize: 14.0,
+                              fontWeight: FontWeight.w500,
+                            ),
+                            textAlign: TextAlign.left
+                        ),
+                        Text(
+                            '199 ₽ в месяц',
+                          style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w500,
+                              color: Color(0x8C000000)),
+                            textAlign: TextAlign.left
+                        ),
+                        SizedBox(width: 150),
+                      ]
+                  ),
+
+                ]
+                  ),
+
+            ),
+
+            Container(
+                margin: const EdgeInsets.only(left: 0, top: 44, right: 10, bottom: 0),
+                child:  Column (
+                    children: <Widget>[
+                      Row (
+                          children: <Widget>[
+                            Image.asset('assets/images/2.png', width: 40, height: 40),
+                            const SizedBox(width: 10),
+                            const Text(
+                                'Переводы',
+                                style: TextStyle(fontSize: 18.0,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                                textAlign: TextAlign.left
+                            )  ] ),
+                      const SizedBox(height: 22),
+                      const Column(
+                          crossAxisAlignment: CrossAxisAlignment.start ,
+                          children: <Widget>[
+                            Text(
+                                'Платёж 9 июля',
+                                style: TextStyle(fontSize: 14.0,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                                textAlign: TextAlign.left
+                            ),
+                            Text(
+                                '199 ₽ в месяц',
+                                style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w500,
+                                    color: Color(0x8C000000)),
+                                textAlign: TextAlign.left
+                            ),
+                            SizedBox(width: 50),
+                          ]
+                      ),
+
                     ]
                 )
-            )
+
+            ),
+
+         ]))
+
           ]
-        )
-      ),
+                )
+            )
+
     );
   }
 }
-
